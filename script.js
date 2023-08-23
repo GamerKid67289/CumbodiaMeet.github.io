b303f7a3ec3ff4621a0c11a684d1271d052c03f1
RobloxGaming <132522767+GamerKid67289@users.noreply.github.com>
2023-08-23 11:40:06 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,47 @@
 const localVideo = document.getElementById('localVideo');
-const remoteVideos = document.getElementById('remoteVideos');
-const toggleCameraButton = document.getElementById('toggleCamera');
-let localStream = null;
+const remoteVideo = document.getElementById('remoteVideo');
+const startButton = document.getElementById('startButton');
+const endButton = document.getElementById('endButton');
+let localStream;
+let peerConnection;
 
-async function startLocalVideo() {
+startButton.addEventListener('click', startCall);
+endButton.addEventListener('click', endCall);
+
+async function startCall() {
     try {
         localStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
         localVideo.srcObject = localStream;
+        peerConnection = new RTCPeerConnection();
+
+        localStream.getTracks().forEach(track => peerConnection.addTrack(track, localStream));
+
+        peerConnection.onicecandidate = event => {
+            if (event.candidate) {
+                // Send the candidate to the remote peer using signaling method
+            }
+        };
+
+        peerConnection.ontrack = event => {
+            remoteVideo.srcObject = event.streams[0];
+        };
+
+        // Create offer and set local description
+        const offer = await peerConnection.createOffer();
+        await peerConnection.setLocalDescription(offer);
+
+        // Send the offer to the remote peer using signaling method
     } catch (error) {
-        console.error('Error accessing camera:', error);
+        console.error('Error starting call:', error);
     }
 }
 
-toggleCameraButton.addEventListener('click', async () => {
-    if (localStream) {
-        const videoTrack = localStream.getVideoTracks()[0];
-        const enabled = !videoTrack.enabled;
-        videoTrack.enabled = enabled;
-        toggleCameraButton.textContent = enabled ? 'Disable Camera' : 'Enable Camera';
-    }
-});
-
-async function initialize() {
-    await startLocalVideo();
-
-    // Additional logic to establish connections and handle remote streams would go here
+async function endCall() {
+    localStream.getTracks().forEach(track => track.stop());
+    localVideo.srcObject = null;
+    remoteVideo.srcObject = null;
+    peerConnection.close();
+    peerConnection = null;
 }
 
-initialize();
+// Signaling methods and handling remote offer/answer and ICE candidates should be implemented here
