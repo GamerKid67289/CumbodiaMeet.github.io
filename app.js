d1f925ca682ed408e3c5d2ac1460dd7ec73c0253
RobloxGaming <132522767+GamerKid67289@users.noreply.github.com>
2023-08-23 12:24:44 -0400

    Update app.js

--- app.js
+++ app.js
@@ -5,11 +5,49 @@ const toggleMicButton = document.getElementById('toggleMic');
 let isCameraOn = true;
 let isMicOn = true;
 
-// Access user media
+let peerConnection;
+const socket = io(); // Assuming you have the socket.io library included
+
+// Access user media and start signaling
 async function startMedia() {
     try {
         const stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
         localVideo.srcObject = stream;
+
+        // Set up peer connection
+        const configuration = { iceServers: [{ urls: 'stun:stun.l.google.com:19302' }] };
+        peerConnection = new RTCPeerConnection(configuration);
+
+        stream.getTracks().forEach(track => peerConnection.addTrack(track, stream));
+
+        // Handle incoming tracks from remote peers
+        peerConnection.ontrack = (event) => {
+            const remoteVideo = document.createElement('video');
+            remoteVideo.srcObject = event.streams[0];
+            remoteVideo.autoplay = true;
+            document.getElementById('videos').appendChild(remoteVideo);
+        };
+
+        // Signal ICE candidates to remote peers
+        peerConnection.onicecandidate = event => {
+            if (event.candidate) {
+                socket.emit('ice-candidate', event.candidate);
+            }
+        };
+
+        // Set up signaling listeners
+        socket.on('ice-candidate', async candidate => {
+            try {
+                await peerConnection.addIceCandidate(candidate);
+            } catch (e) {
+                console.error('Error adding ice candidate:', e);
+            }
+        });
+
+        // Create and send offer to remote peers
+        const offer = await peerConnection.createOffer();
+        await peerConnection.setLocalDescription(offer);
+        socket.emit('offer', offer);
     } catch (error) {
         console.error('Error accessing media devices:', error);
     }
@@ -31,5 +69,5 @@ toggleMicButton.addEventListener('click', () => {
     });
 });
 
-// Start the media on page load
+// Start the media and signaling on page load
 startMedia();
