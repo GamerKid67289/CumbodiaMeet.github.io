17e06599d334ef2a59eeb8c26d4dbb43a8e60b39
RobloxGaming <132522767+GamerKid67289@users.noreply.github.com>
2023-08-23 11:18:52 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,101 +1,42 @@
 const localVideo = document.getElementById("localVideo");
 const remoteVideo = document.getElementById("remoteVideo");
+const remoteVideosContainer = document.getElementById("remoteVideos"); // Add this line
 const startButton = document.getElementById("startButton");
 const hangupButton = document.getElementById("hangupButton");
-const toggleCameraButton = document.getElementById("toggleCameraButton"); // Add this line
-
+const toggleCameraButton = document.getElementById("toggleCameraButton");
 let localStream;
 let remoteStream;
 let pc1;
 let pc2;
-let isCameraOn = true; // Add this line
+let pc3; // Add more peer connections as needed
+let isCameraOn = true;
 
 startButton.addEventListener("click", startCall);
 hangupButton.addEventListener("click", hangupCall);
-toggleCameraButton.addEventListener("click", toggleCamera); // Add this line
-
-async function toggleCamera() { // Add this function
-    if (localStream) {
-        const videoTrack = localStream.getVideoTracks()[0];
-        if (videoTrack) {
-            isCameraOn = !isCameraOn;
-            videoTrack.enabled = isCameraOn;
-            if (isCameraOn) {
-                toggleCameraButton.textContent = "Turn Off Camera";
-            } else {
-                toggleCameraButton.textContent = "Turn On Camera";
-            }
-        }
-    }
-}
-
-// ... (previous code) ...
-
-
+toggleCameraButton.addEventListener("click", toggleCamera);
 
 async function startCall() {
     startButton.disabled = true;
     hangupButton.disabled = false;
+    toggleCameraButton.disabled = false;
 
-    // Get local video stream
-    try {
-        localStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
-        localVideo.srcObject = localStream;
-    } catch (error) {
-        console.error("Error accessing local media:", error);
-        return;
-    }
+    // ... (previous code)
 
-    // Set up PeerConnection
-    const configuration = { iceServers: [{ urls: "stun:stun.l.google.com:19302" }] };
-    pc1 = new RTCPeerConnection(configuration);
-    pc2 = new RTCPeerConnection(configuration);
+    // Create peer connections for each participant
+    participants["self"] = pc1;
+    participants["participant1"] = pc2;
+    participants["participant2"] = pc3; // Add more participants as needed
 
-    // Add local stream to pc1
-    localStream.getTracks().forEach(track => pc1.addTrack(track, localStream));
-
-    // Set up ice candidate handlers for both peers
-    pc1.onicecandidate = event => {
-        if (event.candidate) {
-            pc2.addIceCandidate(event.candidate);
-        }
-    };
-    pc2.onicecandidate = event => {
-        if (event.candidate) {
-            pc1.addIceCandidate(event.candidate);
-        }
-    };
-
-    // Set up remote stream handler for pc2
-    pc2.ontrack = event => {
-        if (!remoteStream) {
-            remoteStream = new MediaStream();
-            remoteVideo.srcObject = remoteStream;
-        }
-        remoteStream.addTrack(event.track);
-    };
-
-    // Create offer and set it as pc1's local description
-    try {
-        const offer = await pc1.createOffer();
-        await pc1.setLocalDescription(offer);
-        await pc2.setRemoteDescription(offer);
-
-        const answer = await pc2.createAnswer();
-        await pc2.setLocalDescription(answer);
-        await pc1.setRemoteDescription(answer);
-    } catch (error) {
-        console.error("Error creating or setting description:", error);
-    }
+    // ... (previous code)
 }
 
-function hangupCall() {
-    pc1.close();
-    pc2.close();
-    localStream.getTracks().forEach(track => track.stop());
-    localVideo.srcObject = null;
-    remoteVideo.srcObject = null;
-    startButton.disabled = false;
-    hangupButton.disabled = true;
+// Function to handle remote stream and display in a remote video element
+function handleRemoteStream(stream, participantId) {
+    const remoteVideoElement = document.createElement("video");
+    remoteVideoElement.id = `remoteVideo_${participantId}`;
+    remoteVideoElement.autoplay = true;
+    remoteVideoElement.srcObject = stream;
+    remoteVideosContainer.appendChild(remoteVideoElement);
 }
 
+// ... (previous code)
