88a2adcee41dc98994e6b70290d84463730d1f74
RobloxGaming <132522767+GamerKid67289@users.noreply.github.com>
2023-08-24 11:25:54 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,44 @@
 const localVideo = document.getElementById('local-video');
+const remoteContainer = document.getElementById('remote-container');
 const toggleCameraButton = document.getElementById('toggle-camera');
 const toggleMicButton = document.getElementById('toggle-mic');
+
+let localStream;
 let cameraOn = true;
 let micOn = true;
 
-async function getMediaStream() {
+async function startLocalStream() {
     try {
-        const stream = await navigator.mediaDevices.getUserMedia({ video: cameraOn, audio: micOn });
-        localVideo.srcObject = stream;
+        localStream = await navigator.mediaDevices.getUserMedia({ video: cameraOn, audio: micOn });
+        localVideo.srcObject = localStream;
     } catch (error) {
         console.error('Error accessing media devices:', error);
     }
 }
 
+// Toggle camera
 toggleCameraButton.addEventListener('click', () => {
     cameraOn = !cameraOn;
-    getMediaStream();
+    localStream.getVideoTracks()[0].enabled = cameraOn;
 });
 
+// Toggle microphone
 toggleMicButton.addEventListener('click', () => {
     micOn = !micOn;
-    getMediaStream();
+    localStream.getAudioTracks()[0].enabled = micOn;
 });
 
-getMediaStream();
+// Function to add a new remote video
+function addRemoteVideo(stream) {
+    const remoteVideo = document.createElement('video');
+    remoteVideo.srcObject = stream;
+    remoteVideo.autoplay = true;
+    remoteContainer.appendChild(remoteVideo);
+}
+
+// Handle signaling and peer connections - you need to implement this part
+
+// Call this function to add a remote stream
+// For example: addRemoteVideo(remoteStream);
+
+startLocalStream();
