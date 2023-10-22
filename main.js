06ea91391221e17743ab9e1b4862931c69a213de
RobloxGaming <132522767+GamerKid67289@users.noreply.github.com>
2023-10-22 10:26:56 -0400

    Update main.js

--- main.js
+++ main.js
@@ -2,20 +2,18 @@ const APP_ID = "25540f7394b841d29dfe60b86c43a5eb"
 const TOKEN = "007eJxTYJjt17nppU3GDg6mkImXnZ7u/+p/72zT0jbBJYd+3QsqevhTgcHI1NTEIM3c2NIkycLEMMXIMiUt1cwgycIs2cQ40TQ1SSjDJLUhkJHBdr0aKyMDBIL4HAzOpblJ+SmZiQwMAH+jInA="
 const CHANNEL = "Cumbodia"
 
-const client = AgoraRTC.createClient({mode:'rtc', codec:'vp8'})
+const client = AgoraRTC.createClient({ mode: 'rtc', codec: 'vp8' })
 
 let localTracks = []
 let remoteUsers = {}
 
 let joinAndDisplayLocalStream = async () => {
-
     client.on('user-published', handleUserJoined)
-    
     client.on('user-left', handleUserLeft)
-    
+
     let UID = await client.join(APP_ID, CHANNEL, TOKEN, null)
 
-    localTracks = await AgoraRTC.createMicrophoneAndCameraTracks() 
+    localTracks = await AgoraRTC.createMicrophoneAndCameraTracks()
 
     let player = `<div class="video-container" id="user-container-${UID}">
                         <div class="video-player" id="user-${UID}"></div>
@@ -23,8 +21,8 @@ let joinAndDisplayLocalStream = async () => {
     document.getElementById('video-streams').insertAdjacentHTML('beforeend', player)
 
     localTracks[1].play(`user-${UID}`)
-    
-    await client.publish([localTracks[0], localTracks[1]])
+
+    await client.publish([localTracks[0], localTracks[1])
 }
 
 let joinStream = async () => {
@@ -34,12 +32,12 @@ let joinStream = async () => {
 }
 
 let handleUserJoined = async (user, mediaType) => {
-    remoteUsers[user.uid] = user 
+    remoteUsers[user.uid] = user
     await client.subscribe(user, mediaType)
 
-    if (mediaType === 'video'){
+    if (mediaType === 'video') {
         let player = document.getElementById(`user-container-${user.uid}`)
-        if (player != null){
+        if (player != null) {
             player.remove()
         }
 
@@ -51,7 +49,7 @@ let handleUserJoined = async (user, mediaType) => {
         user.videoTrack.play(`user-${user.uid}`)
     }
 
-    if (mediaType === 'audio'){
+    if (mediaType === 'audio') {
         user.audioTrack.play()
     }
 }
@@ -62,23 +60,23 @@ let handleUserLeft = async (user) => {
 }
 
 let leaveAndRemoveLocalStream = async () => {
-    for(let i = 0; localTracks.length > i; i++){
+    for (let i = 0; localTracks.length > i; i++) {
         localTracks[i].stop()
         localTracks[i].close()
     }
 
     await client.leave()
     document.getElementById('join-btn').style.display = 'block'
-    document.getElementById('stream-controls').style.display = 'none'
+    document.getElementById('stream-controls').style display = 'none'
     document.getElementById('video-streams').innerHTML = ''
 }
 
 let toggleMic = async (e) => {
-    if (localTracks[0].muted){
+    if (localTracks[0].muted) {
         await localTracks[0].setMuted(false)
         e.target.innerText = 'Mic on'
         e.target.style.backgroundColor = 'cadetblue'
-    }else{
+    } else {
         await localTracks[0].setMuted(true)
         e.target.innerText = 'Mic off'
         e.target.style.backgroundColor = '#EE4B2B'
@@ -86,18 +84,53 @@ let toggleMic = async (e) => {
 }
 
 let toggleCamera = async (e) => {
-    if(localTracks[1].muted){
+    if (localTracks[1].muted) {
         await localTracks[1].setMuted(false)
         e.target.innerText = 'Camera on'
         e.target.style.backgroundColor = 'cadetblue'
-    }else{
+    } else {
         await localTracks[1].setMuted(true)
         e.target.innerText = 'Camera off'
         e.target.style.backgroundColor = '#EE4B2B'
     }
 }
 
-document.getElementById('join-btn').addEventListener('click', joinStream)
-document.getElementById('leave-btn').addEventListener('click', leaveAndRemoveLocalStream)
-document.getElementById('mic-btn').addEventListener('click', toggleMic)
-document.getElementById('camera-btn').addEventListener('click', toggleCamera)
+// Add the following code for the chat feature
+
+// Get references to chat elements
+const chatMessages = document.getElementById('chat-messages')
+const chatInput = document.getElementById('chat-input')
+const sendBtn = document.getElementById('send-btn')
+
+// Function to send a chat message
+let sendChatMessage = (message) => {
+    const chatMessage = {
+        sender: 'You', // Replace 'You' with the actual sender name
+        message: message
+    };
+
+    displayChatMessage(chatMessage);
+
+    // Here, you can send the message to other users via your video chat platform's API
+    // Implement this part depending on your video chat platform
+    // Replace the following line with the actual code to send the message.
+    sendChatMessageToOtherUsers(chatMessage.message);
+};
+
+// Function to display a chat message in the chat interface
+let displayChatMessage = (chatMessage) => {
+    const messageDiv = document.createElement('div');
+    messageDiv.innerText = `${chatMessage.sender}: ${chatMessage.message}`;
+    chatMessages.appendChild(messageDiv);
+};
+
+// Event listener for sending a chat message
+sendBtn.addEventListener('click', () => {
+    const message = chatInput.value;
+    if (message.trim() !== '') {
+        sendChatMessage(message);
+        chatInput.value = '';
+    }
+});
+
+// ... Your existing code ...
